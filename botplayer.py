5290a53fa04617abf2b7b5b999edd6f6d669fb7f
Javiereski <javieresky@gmail.com>
2016-10-08 01:21:28 -0300

    Javier
    Botplayer

--- botplayer.py
+++ botplayer.py
@@ -1,52 +1,19 @@
 import numpy
 from cliente_alumnos import posicion
-global posicionActual
 
-def escoger_movimiento( amenazas ):
-    ambos = ambos.strip().split(':')
-    amenaza = ambos[0].strip().split('-')
-    cuadrante = ambos[1].strip().split('-')
-    del amenaza[0]
+def escoger_movimiento(amenazas):
     movimiento_x = ""
-    movimiento_y = ""
-    return movimiento_x + "," + movimiento_y
-
-def escoger_disparo( amenazas ):
-    ambos = ambos.strip().split(':')
-    amenaza = ambos[0].strip().split('-')
-    cuadrante = ambos[1].strip().split('-')
-    del amenaza[0]
-    
+    movimiento_y = "" 
+    return movimiento_x + "," + movimiento_y    
+def escoger_disparo(amenazas):
+	a = amenazas.split(":")
+    amenaza = a[0].split("-") # Lista de amenazas [a, g1, g2, g3, gn]
+    del amenaza[0] # Lista de amenazas [g1, g2, g3, gn]
+    cuadrante = a[1].split("-") # Lista de cuadrantes [c1, c2, c3, c4]
     disparo_x = ""
     disparo_y = ""
     return disparo_x + "," + disparo_y
 
-posicionActual = dict()
-
-
-
-
-#Si la posicion es 0 o 9 estamos en una esquina
-#en estos puntos distintos cuadrantes y se pierden algunos
-
-
-
-##Disparo:
-##  -  si 1 cuadrante tiene a todos los wnes y si hay 2 grados de amenaza
-##iguales, disparar a eso, 100% chance de golpear.
-##  - si hay 2 cuadrantes con jugadores, elegir el que tenga mayor jugadores
-##y disparar a la amenaza existida(o mas repetida).
-##  - seguir la misma logica para 3 y 4 cuadrantes.
-##condiciones para cuando estas en los bordes, se elminan cuadrantes en estos casos
-
-##cuando esta en una esquina y se pierde un cuadrante, entrega 0
-
-
-
-##movimiento:
-##    - si no hay amenaza, revisar cuadrantes y moverse a donde hay menor cantidad
-##de enemigos. (intentar no chocar)
-
-
-
-
+# global posiciones = {} # LLave = N° de turno, Valor = lista de 2 elementos, Posicion (lista con 2 elementos) y amenazas.
+#Criterios de disparo
+#Criterios de movimiento 
