f12af366a5b34dbfe0243ca508e71b3e76519da2
Javiereski <javieresky@gmail.com>
2016-10-08 22:51:24 -0300

    ultimate

---
+++ the-ultimate-bot-player.py
@@ -0,0 +1,140 @@
+import numpy
+import random
+from cliente_alumnos import posicion
+global posicionActual
+posicionActual = dict()
+def escoger_movimiento( amenazas ):
+    grados,cuadrantes=amenazas.strip().split(':')
+    grados=grados.split('-') #el primer elemento es 'a'
+    cuadrantes=cuadrantes.split('-')
+    cuadrantes=map(int, cuadrantes)
+    minc=min(cuadrantes)
+    if minc == cuadrantes[0]:
+        if cuadrantes[1]<cuadrantes[2] and cuadrantes[1]<=cuadrantes[0]:
+
+    if minc == cuadrantes[1]:
+        if cuadrantes[2]<cuadrantes[3]:
+        
+    if minc == cuadrantes[2]:
+        if cuadrantes[3]< cuadrantes[0]
+        movimiento_y='1'
+    elif minc ==cuadrantes[1]:
+        movimiento_x='-1'
+    elif minc==cuadrantes[2]:
+        movimiento_y='-1'
+    elif minc==cuadrantes[3]:
+        movimiento_x='1'
+    
+    movimiento_x = ""
+    movimiento_y = ""
+return movimiento_x + "," + movimiento_y
+
+def escoger_disparo( amenazas ):
+    ambos = ambos.strip().split(':')
+    amenaza = ambos[0].strip().split('-') #Lista de amenzas [g1, g2, g3, gn]
+    cuadrante = ambos[1].strip().split('-') #Lista de cuadrantes [c1, c2, c3, c4]    
+    del amenaza[0]
+    amenaza.sort()
+    c1 = cuadrante[0] #Cuadrantes en valores individuales 
+    c2 = cuadrante[1]
+    c3 = cuadrante[2]
+    c4 = cuadrante[3]
+    if   c1 != "0" and (c2 == c3 == c4 == "0"):
+        disparo_x, disparo_y = disparo_seguro(amenaza, "1")
+    elif c2 != "0" and (c1 == c3 == c4 == "0"):
+        disparo_x, disparo_y = disparo_seguro(amenaza, "2")
+    elif c3 != "0" and (c1 == c2 == c4 == "0"):
+        disparo_x, disparo_y = disparo_seguro(amenaza, "3")
+    elif c4 != "0" and (c1 == c2 == c2 == "0"):
+        disparo_x, disparo_y = disparo_seguro(amenaza, "4")
+
+
+
+
+
+    return disparo_x + "," + disparo_y
+
+def disparo_seguro(amenaza, cuadrante): # Recibe el cuadrante y la lista de amenzas ordenadas, hecho para no poner todo esto en la funcion escoger_disparo
+    count = 0
+    if cuadrante == "1": # Todos los enemigos estan en cuadrante 1
+        while count < len(amenaza):
+            if amenaza[c] == amenaza[c+1]:
+                disparo_y = "0"
+                if amenaza[c] == "1":
+                    disparo_x = random.choice("4", "5")
+                    break
+                elif amenaza[c] == "2":
+                    disparo_x = random.choice("2", "3")
+                    break
+                else:
+                    disparo_x = "1"
+            else:
+                c += 1  
+    elif cuadrante == "2": # Todos los enemigos estan en cuadrante 2
+        while count < len(amenaza):
+            if amenaza[c] == amenaza[c+1]:
+                disparo_x = "0"
+                if amenaza[c] == "1":
+                    disparo_y = random.choice("-4", "-5")
+                    break
+                elif amenaza[c] == "2":
+                    disparo_y = ranodm.choice("-3", "-2")
+                    break
+                else:
+                    disparo_y = "-1"
+                    break
+            else:
+                c +=1   
+    elif cuadrante == "3": # Todos los enemigos estan en cuadrante 3
+        while count < len(amenaza):
+            if amenaza[c] == amenaza[c+1]:
+                disparo_y = "0"
+                if amenaza[c] == "1":
+                    disparo_x = random.choice("-4", "-5")
+                    break
+                elif amenaza[c] == "2":
+                    disparo_x = random.choice("-3", "-2")
+                    break
+                else:
+                    disparo_x = "-1"
+                    break
+            else:
+                c += 1
+    else:
+        while count < len(amenaza):
+            if amenaza[c] == amenaza[c+1]:
+                disparo_x = "0"
+                if amenaza[c] == "1":
+                    disparo_y = random.choice("4", "5")
+                    break 
+                elif amenaza[c] == "2":
+                    disparo_y = random.choice("2", "3")
+                    break
+                else:
+                    disparo_y = "1"
+                    break
+            else:
+                c += 1
+    return disparo_x, disparo_y         
+
+
+
+
+#Si la posicion es 0 o 9 estamos en una esquina
+#en estos puntos distintos cuadrantes y se pierden algunos
+
+
+
+##Disparo:
+##  - si hay 2 cuadrantes con jugadores, elegir el que tenga mayor jugadores
+##y disparar a la amenaza existida(o mas repetida).
+##  - seguir la misma logica para 3 y 4 cuadrantes.
+##condiciones para cuando estas en los bordes, se elminan cuadrantes en estos casos
+##cuando esta en una esquina y se pierde un cuadrante, entrega 0
+
+
+##movimiento:
+##    - si no hay amenaza, revisar cuadrantes y moverse a donde hay menor cantidad
+##de enemigos. (intentar no chocar)
+# ocupar la posición inicial para calcular la consentracion de jugadores respecto al area de cada caudrante
+
